92e79f23c5509acc47e55e9c25ecde54201b2442
pyegork <egorkoromyslov@gmail.com>
2023-10-24 19:03:13 +0300

    handlers_params

---
+++ src/handlers/_handlers.py
@@ -0,0 +1,50 @@
+import asyncio
+
+from aiogram import types
+from aiogram.dispatcher import FSMContext
+
+from pathlib import Path
+
+from ..constants import DATA_DIR
+from ..common import dp
+from ..models import db
+from ._utils import kb, texts, states
+
+
+@dp.message_handler(commands=['start'], state='*')
+async def start(message: types.Message):
+    await db.registrate_if_not_exists(message.from_user.id)
+    await db.update_stage(message.from_user.id, 'stage_1')
+    await message.answer(texts.welcome, reply_markup=kb.start)
+
+
+@dp.message_handler(lambda message: message.text == "🌙Таро расклад общее состояние", state='*')
+async def start_choosing_cards(message: types.Message, state: FSMContext):
+    await db.update_stage(message.from_user.id, 'stage_2')
+    f = types.InputFile(DATA_DIR / 'taro_cards.jpg')
+    await message.answer_photo(f, caption=texts.choose_taro_card, reply_markup=kb.choosing_taro_card)
+
+
+@dp.message_handler(lambda message: message.text in kb.taro_cards_title, state='*')
+async def choose_card(message: types.Message, state: FSMContext):
+    await message.answer('🪄Обрабатываю информацию...', reply_markup=types.ReplyKeyboardRemove())
+    choose = int(message.text.split()[-1]) - 1
+    asyncio.create_task(send_late_taro_analyze(message, choose))
+
+
+def get_taro_card(user_choice: int):
+    if len(texts.taro_cards) - 1 < user_choice:
+        user_choice = -1
+    photo: Path = texts.taro_cards[user_choice]['photo']
+    text: str = texts.taro_cards[user_choice]['text']
+    return photo, text
+
+
+async def send_late_taro_analyze(message: types.Message, user_choose: int):
+    await asyncio.sleep(4.5)
+    if len(texts.taro_cards) - 1 < user_choose:
+        user_choose = -1
+    photo, text = get_taro_card(user_choose)
+    await db.update_stage(message.from_user.id, 'stage_3')
+    file = types.InputFile(photo)
+    await message.answer_photo(file, caption=text, reply_markup=kb.to_autoanswer, parse_mode='html')
---
+++ src/handlers/_utils/kb.py
@@ -0,0 +1,38 @@
+import json
+
+from itertools import islice
+from aiogram import types
+
+
+def _get_titles_from_kb(kb: types.ReplyKeyboardMarkup):
+    json_kb = json.loads(kb.as_json())['keyboard']
+    titles = []
+    for row in json_kb:
+        for btn in row:
+            titles.append(btn['text'])
+    return titles
+
+
+def _chunk(it, size):
+    it = iter(it)
+    return iter(lambda: tuple(islice(it, size)), ())
+
+
+def __init_choosing_taro_card():
+    for tuple_of_indexes in _chunk(range(1, 7), 2):  # Карта Таро
+        btns = [types.KeyboardButton(f'Карта Таро {index}') for index in tuple_of_indexes]
+        choosing_taro_card.add(*btns)
+
+
+start = types.ReplyKeyboardMarkup(resize_keyboard=True)
+start.add(types.KeyboardButton('🌙Таро расклад общее состояние'))
+
+
+# Клавиатура при выборе таро карты
+choosing_taro_card = types.ReplyKeyboardMarkup(resize_keyboard=True)
+__init_choosing_taro_card()
+taro_cards_title = _get_titles_from_kb(choosing_taro_card)
+
+
+to_autoanswer = types.InlineKeyboardMarkup()
+to_autoanswer.add(types.InlineKeyboardButton('🔮 Получить расклад', url='https://t.me/taro_anna_pie'))
